5070e9090e134d6011d6ab6c846ba4c26aa3e964
Fidalgobr <82066907+Fidalgobr@users.noreply.github.com>
2021-09-14 22:35:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
+
+async function action(d) {
+    const data = d.data;
+    const $ = cheerio.load(data);
+    let vezes = 0;
+
+    for (let sr of $('.box-banner img')) {
+        const imgSrc = sr.attribs.src;
+        if (/\/ids\//g.test(imgSrc)) {
+            const dooEt = async() => {
+                let errorParam = false;
+                await axios.get(imgSrc, { responseType: 'stream' }).then(r => {
+                    r.data.pipe(fs.createWriteStream(path.join(__dirname, 'arquivos/celebridades', `imagem${vezes}.jpg`)));
+                    vezes++;
+                }).catch(e => {
+                    error = true;
+                });
+                return errorParam;
+            }
+
+            let resultadoErr = await dooEt();
+            while (resultadoErr) {
+                resultadoErr = await dooEt();
+            }
+            if (!resultadoErr) {
+                console.log(`Sucesso! ${vezes}`)
+            }
+        } else {
+            continue;
+        }
+    }
+
+}
+
+axios.get('https://www.macrobaby.com/celebrities').then(d => {
+        action(d)
+    })
+    .catch(e => console.log('Erro!'))
